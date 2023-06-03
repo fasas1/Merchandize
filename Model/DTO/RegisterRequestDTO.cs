f84eb630aef2e4a11ca5a1edd3f90da7a851394a
Fasasi Qasim <fasaqa1000@gmail.com>
2023-06-03 09:41:04 +0100

    RegisterRequestDTO

---
+++ Model/DTO/RegisterRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Merchantdized.Model.DTO
+{
+    public class RegisterRequestDTO
+    {
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
