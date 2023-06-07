e3367de3435a0e080f1e0c5864a051440fdf12ba
Fasasi Qasim <fasaqa1000@gmail.com>
2023-06-07 08:07:24 +0100

    fixes

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -50,29 +50,29 @@ namespace Rellish.Controllers
                 _response.ErrorMessages.Add("Username already Exixts!");
                 return BadRequest(_response);
             }
-            Generate JWT Token
-            //var roles = await _userManager.GetRolesAsync(userFromDb);
+           // Generate JWT Token
+            var roles = await _userManager.GetRolesAsync(userFromDb);
             JwtSecurityTokenHandler tokenHandler = new();
              byte[] key = Encoding.ASCII.GetBytes(secretKey);
 
-            //SecurityTokenDescriptor tokenDescriptor = new()
-            //{
-            //    Subject = new ClaimsIdentity(new Claim[]
-            //    {
-            //        new Claim("fullname", userFromDb.Name),
-            //        new Claim("id", userFromDb.Id.ToString()),
-            //        new Claim(ClaimTypes.Email, userFromDb.UserName.ToString()),
-            //        new Claim(ClaimTypes.Role, roles.FirstOrDefault()),
-            //    }),
-            //    Expires = DateTime.UtcNow.AddDays(7),
-            //    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            SecurityTokenDescriptor tokenDescriptor = new()
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim("fullname", userFromDb.Name),
+                    new Claim("id", userFromDb.Id.ToString()),
+                    new Claim(ClaimTypes.Email, userFromDb.UserName.ToString()),
+                    new Claim(ClaimTypes.Role, roles.FirstOrDefault()),
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
 
-            //};
-            //SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+            };
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
             LoginResponseDTO loginResponse = new()
             {
                 Email = userFromDb.Email,
-                Token = "No value yet"  /*tokenHandler.WriteToken(token)*/
+                Token = tokenHandler.WriteToken(token)
             };
 
             if (loginResponse.Email == null || string.IsNullOrEmpty(loginResponse.Token))
