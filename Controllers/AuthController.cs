22587ba7f307e0731c1a41b084e3cc605c03e6f5
Fasasi Qasim <fasaqa1000@gmail.com>
2023-06-08 04:19:09 +0100

    fixes

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -29,60 +29,7 @@ namespace Rellish.Controllers
             _userManager = userManager;
         }
 
-        //[HttpPost("login")]
-        //public async Task<IActionResult> Login([FromBody] LoginRequestDTO model)
-        //{
-        //    ApplicationUser userFromDb = _db.ApplicationUsers.FirstOrDefault
-        //        (u => u.UserName.ToLower() == model.UserName.ToLower());
-
-        //    bool isValid = await _userManager.CheckPasswordAsync(userFromDb, model.Password);
-
-
-        //    if (isValid == false)
-        //    {
-        //        _response.Result = new LoginRequestDTO();
-        //        _response.StatusCode = HttpStatusCode.BadRequest;
-        //        _response.IsSuccess = false;
-        //        _response.ErrorMessages.Add("Username already Exixts!");
-        //        return BadRequest(_response);
-        //    }
-        //    //Generate JWT Token
-        //    var roles = await _userManager.GetRolesAsync(userFromDb);
-        //    JwtSecurityTokenHandler tokenHandler = new();
-        //    byte[] key = Encoding.ASCII.GetBytes(secretKey);
-
-        //    SecurityTokenDescriptor tokenDescriptor = new()
-        //    {
-        //        Subject = new ClaimsIdentity(new Claim[]
-        //        {
-        //            new Claim("fullname", userFromDb.Name),
-        //            new Claim("id", userFromDb.Id.ToString()),
-        //            new Claim(ClaimTypes.Email, userFromDb.UserName.ToString()),
-        //            new Claim(ClaimTypes.Role, roles.FirstOrDefault()),
-        //        }),
-        //        Expires = DateTime.UtcNow.AddDays(7),
-        //        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-
-        //    };
-        //    SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
-        //    LoginResponseDTO loginResponse = new()
-        //    {
-        //        Email = userFromDb.Email,
-        //        Token = tokenHandler.WriteToken(token)
-        //    };
-
-        //    if (loginResponse.Email == null || string.IsNullOrEmpty(loginResponse.Token))
-        //    {
-        //        _response.StatusCode = HttpStatusCode.BadRequest;
-        //        _response.IsSuccess = false;
-        //        _response.ErrorMessages.Add("Username or Password already Exixts!");
-        //        return BadRequest(_response);
-        //    }
-        //    _response.StatusCode = HttpStatusCode.OK;
-        //    _response.IsSuccess = true;
-        //    _response.Result = loginResponse;
-        //    return Ok(_response);
-        //}
+      
 
         [HttpPost]
         public async Task<IActionResult> Register([FromBody] RegisterRequestDTO model)
@@ -131,7 +78,7 @@ namespace Rellish.Controllers
                     return Ok(_response);
                 }
             }
-            catch (Exception)
+            catch (Exception ex)
             {
 
             }
