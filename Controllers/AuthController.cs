9df2276e99ede859121040830f6752d886572e7c
Fasasi Qasim <fasaqa1000@gmail.com>
2023-06-08 03:35:21 +0100

    changes

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,12 +1,14 @@
-﻿using Merchantdized.Data;
-using Merchantdized.Model;
-using Merchantdized.Model.DTO;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
+using System.Text;
+using Merchantdized.Data;
+using Merchantdized.Model.DTO;
+using Merchantdized.Model;
+using Merchantdized.Utility;
 
-namespace Merchantdized.Controllers
+namespace Rellish.Controllers
 {
     [Route("api/auth")]
     [ApiController]
@@ -15,31 +17,131 @@ namespace Merchantdized.Controllers
         private readonly ApplicationDbContext _db;
         private ApiResponse _response;
         private readonly UserManager<ApplicationUser> _userManager;
-        private readonly RoleManager<ApplicationUser> _roleManager;
-        public string secretKey;
-
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private string secretKey;
         public AuthController(ApplicationDbContext db, IConfiguration configuration,
-                              UserManager<ApplicationUser> userManager,
-                              RoleManager<ApplicationUser> roleManager)
+            UserManager<ApplicationUser> userManager,
+         RoleManager<IdentityRole> roleManager)
         {
             _db = db;
-            _response = new ApiResponse();
             secretKey = configuration.GetValue<string>("ApiSettings:Secret");
-            _userManager = userManager;
+            _response = new ApiResponse();
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
-        [HttpPost]
-        public async Task <IActionResult> Register([FromBody] RegisterRequestDTO model)
+        //[HttpPost("login")]
+        //public async Task<IActionResult> Login([FromBody] LoginRequestDTO model)
+        //{
+        //    ApplicationUser userFromDb = _db.ApplicationUsers.FirstOrDefault
+        //        (u => u.UserName.ToLower() == model.UserName.ToLower());
+
+        //    bool isValid = await _userManager.CheckPasswordAsync(userFromDb, model.Password);
+
+
+        //    if (isValid == false)
+        //    {
+        //        _response.Result = new LoginRequestDTO();
+        //        _response.StatusCode = HttpStatusCode.BadRequest;
+        //        _response.IsSuccess = false;
+        //        _response.ErrorMessages.Add("Username already Exixts!");
+        //        return BadRequest(_response);
+        //    }
+        //    //Generate JWT Token
+        //    var roles = await _userManager.GetRolesAsync(userFromDb);
+        //    JwtSecurityTokenHandler tokenHandler = new();
+        //    byte[] key = Encoding.ASCII.GetBytes(secretKey);
+
+        //    SecurityTokenDescriptor tokenDescriptor = new()
+        //    {
+        //        Subject = new ClaimsIdentity(new Claim[]
+        //        {
+        //            new Claim("fullname", userFromDb.Name),
+        //            new Claim("id", userFromDb.Id.ToString()),
+        //            new Claim(ClaimTypes.Email, userFromDb.UserName.ToString()),
+        //            new Claim(ClaimTypes.Role, roles.FirstOrDefault()),
+        //        }),
+        //        Expires = DateTime.UtcNow.AddDays(7),
+        //        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+
+        //    };
+        //    SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+        //    LoginResponseDTO loginResponse = new()
+        //    {
+        //        Email = userFromDb.Email,
+        //        Token = tokenHandler.WriteToken(token)
+        //    };
+
+        //    if (loginResponse.Email == null || string.IsNullOrEmpty(loginResponse.Token))
+        //    {
+        //        _response.StatusCode = HttpStatusCode.BadRequest;
+        //        _response.IsSuccess = false;
+        //        _response.ErrorMessages.Add("Username or Password already Exixts!");
+        //        return BadRequest(_response);
+        //    }
+        //    _response.StatusCode = HttpStatusCode.OK;
+        //    _response.IsSuccess = true;
+        //    _response.Result = loginResponse;
+        //    return Ok(_response);
+        //}
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDTO model)
         {
-            ApplicationUser userFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.UserName.ToLower() == model.UserName.ToLower());
-            if (userFromDb == null)
+            ApplicationUser userFromDb = _db.ApplicationUsers
+                .FirstOrDefault(u => u.UserName.ToLower() == model.UserName.ToLower());
+
+            if (userFromDb != null)
             {
                 _response.StatusCode = HttpStatusCode.BadRequest;
                 _response.IsSuccess = false;
-                _response.ErrorMessages.Add("User Already Exist");
+                _response.ErrorMessages.Add("Username already exists");
                 return BadRequest(_response);
             }
+
+            ApplicationUser newUser = new()
+            {
+                UserName = model.UserName,
+                Email = model.UserName,
+                NormalizedEmail = model.UserName.ToUpper(),
+                Name = model.Name
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(newUser, model.Password);
+                if (result.Succeeded)
+                {
+                    if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
+                    {
+                        //create roles in database
+                        await _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin));
+                        await _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer));
+                    }
+                    if (model.Role.ToLower() == SD.Role_Admin)
+                    {
+                        await _userManager.AddToRoleAsync(newUser, SD.Role_Admin);
+                    }
+                    else
+                    {
+                        await _userManager.AddToRoleAsync(newUser, SD.Role_Customer);
+                    }
+
+                    _response.StatusCode = HttpStatusCode.OK;
+                    _response.IsSuccess = true;
+                    return Ok(_response);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            _response.StatusCode = HttpStatusCode.BadRequest;
+            _response.IsSuccess = false;
+            _response.ErrorMessages.Add("Error while registering");
+            return BadRequest(_response);
+
         }
     }
+
 }
